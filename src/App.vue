1a7075a7e1d431381bce0200f8f435a10f496eaa
ReynaldiRP <rizkypratama471@gmail.com>
2024-03-24 12:51:43 +0700

    [FEAT] add card component

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,33 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <body>
+    <section :class="checkProductSection()" id="background-section">
+      <CardProduct> </CardProduct>
+    </section>
+  </body>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import CardProduct from './components/CardProduct.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+import { ref } from 'vue'
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+const productSection = ref(['women-section'])
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+const checkProductSection = () => {
+  const newValue = productSection.value
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  if (newValue.length > 0) {
+    if (newValue === 'men-section') {
+      productSection.value = 'men-section'
+    } else if (newValue === 'women-section') {
+      productSection.value = 'women-section'
+    }
+    return productSection.value
+  } else {
+    return (productSection.value = 'unavailable-section')
   }
 }
-</style>
+</script>
---
+++ src/components/CardProduct.vue
@@ -0,0 +1,20 @@
+<template>
+  <article :class="checkCardSection()" id="card">
+    <slot></slot>
+  </article>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const cardSection = ref('card-section')
+
+const checkCardSection = () => {
+  const newValue = cardSection.value
+
+  if (newValue == 'card-section') {
+    return (cardSection.value = 'card-section')
+  } else {
+    return (cardSection.value = 'unavailable-card-section')
+  }
+}
+</script>
