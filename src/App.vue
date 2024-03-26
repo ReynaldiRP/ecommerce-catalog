ea6cd429c7133bca95bcf101ae36f77e81567e9c
ReynaldiRP <rizkypratama471@gmail.com>
2024-03-26 17:03:37 +0700

    [feat] add fucntion to change style based on current section

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,110 @@
 <template>
   <body>
-    <MenSection :showNextSection="showNextSection" v-if="currentSection === 'men-section'" />
-    <WomenSection
+    <ProductSection
       :showNextSection="showNextSection"
-      v-else-if="currentSection === 'women-section'"
-      :circleClasses="circleClasses(womenClothes?.value?.rating)"
+      :dataProduct="dataProduct"
+      :circleClasses="circleClasses"
+      :formattedRating="formattedRating"
+      :currentSection="checkCurrentSection"
     />
-    <UnavailableSection :showNextSection="showNextSection" v-else />
+    <!-- <UnavailableSection :showNextSection="showNextSection" v-else /> -->
   </body>
   <!-- <RouterView /> -->
 </template>
 
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import UnavailableSection from '@/components/UnavailableSection.vue'
-import MenSection from '@/components/MenSection.vue'
-import WomenSection from '@/components/WomenSection.vue'
+// import UnavailableSection from '@/components/UnavailableSection.vue'
+import ProductSection from '@/components/ProductSection.vue'
 
-import { ref, computed } from 'vue'
-
-const pageSection = ref(['men-section', 'women-section', 'unavailable-section'])
-const currentIndex = ref(0)
-
-const currentSection = computed(() => {
-  return pageSection.value[currentIndex.value]
+import { ref, computed, onMounted } from 'vue'
+const dataProduct = ref({
+  title: '',
+  category: '',
+  rating: 0,
+  image: '',
+  description: '',
+  price: 0
 })
 
-const showNextSection = () => {
-  currentIndex.value = (currentIndex.value + 1) % pageSection.value.length
+const currentIdProduct = ref(1)
+let className = ref({})
+
+const fetchData = async () => {
+  try {
+    const url = `https://fakestoreapi.com/products/${currentIdProduct.value++}`
+    let response = await fetch(url)
+    let data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return null
+  }
 }
 
-const circleClasses = (rating) => {
+onMounted(async () => {
+  await fetchDataAndUpdate()
+})
+
+const fetchDataAndUpdate = async () => {
+  const data = await fetchData()
+  if (data) {
+    dataProduct.value = {
+      title: data.title,
+      category: data.category,
+      rating: data.rating.rate,
+      image: data.image,
+      description: data.description,
+      price: data.price
+    }
+  }
+}
+
+const circleClasses = computed(() => {
   const classes = []
-  const roundedRating = Math.round(rating)
-  console.log(roundedRating)
-  console.log(roundedRating)
+  const rating = Math.round(dataProduct.value.rating)
   for (let i = 0; i < 5; i++) {
-    classes.push({ circle: true, active: i < roundedRating })
+    classes.push({ circle: true, active: i < rating })
   }
   return classes
+})
+
+const formattedRating = computed(() => {
+  return `${dataProduct.value.rating}/5`
+})
+
+const checkCurrentSection = () => {
+  const category = dataProduct.value.category
+  if (category === `men's clothing`) {
+    return (className.value = {
+      section: 'men-section',
+      title: 'men-clothes-title',
+      rating: 'circle-men',
+      price: 'men-clothes-price',
+      buttonBuy: 'men-button-buy',
+      buttonNext: 'men-button-next'
+    })
+  } else if (category === `women's clothing`) {
+    return (className.value = {
+      section: 'women-section',
+      title: 'women-clothes-title',
+      rating: 'circle-women',
+      price: 'women-clothes-price',
+      buttonBuy: 'women-button-buy',
+      buttonNext: 'women-button-next'
+    })
+  }
+  return (className.value = {
+    section: 'unavailable-section',
+    title: 'unavailable-primary-text',
+    rating: 'circle-unavailable',
+    price: 'unvailable-clothes-price',
+    buttonBuy: 'unavailable-button-buy',
+    buttonNext: 'unavailable-button-next'
+  })
+}
+
+const showNextSection = async () => {
+  await fetchDataAndUpdate()
 }
 </script>
---
+++ src/components/ProductSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <section :class="currentSection().section" id="background-section">
+    <CardProduct class="card-section">
+      <img :src="dataProduct.image" class="skeleton-img" id="clothesImage" />
+      <article class="clothes-description">
+        <h1 :class="currentSection().title" id="clothes-title">
+          {{ dataProduct.title }}
+        </h1>
+        <section class="clothes-categories">
+          <p class="clothes-category">{{ dataProduct.category }}</p>
+          <section class="clothes-rating">
+            <p>{{ formattedRating }}</p>
+            <div class="rating-indicator">
+              <div
+                v-for="(circle, index) in circleClasses"
+                :key="index"
+                :class="[circle, currentSection().rating]"
+              ></div>
+            </div>
+          </section>
+        </section>
+        <div class="border-bottom"></div>
+        <p class="clothes-description">
+          {{ dataProduct.description }}
+        </p>
+        <section class="clothes-footer">
+          <div class="border-bottom"></div>
+          <h1 :class="currentSection().price" id="clothes-price">${{ dataProduct.price }}</h1>
+          <div class="button-group">
+            <button :class="currentSection().buttonBuy">Buy now</button>
+            <button @click="showNextSection" :class="currentSection().buttonNext">
+              Next Product
+            </button>
+          </div>
+        </section>
+      </article>
+    </CardProduct>
+  </section>
+</template>
+
+<script setup>
+import CardProduct from '@/components/CardProduct.vue'
+
+defineProps({
+  showNextSection: Function,
+  dataProduct: Object,
+  circleClasses: Array,
+  formattedRating: String,
+  currentSection: Function
+})
+</script>
