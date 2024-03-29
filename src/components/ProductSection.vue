06c6040838de1782c67f101e9a98238dabeca068
ReynaldiRP <rizkypratama471@gmail.com>
2024-03-29 10:57:31 +0700

    [feat] add state management for all state and function

--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -1,152 +1,67 @@
 <script setup>
+import { useProductSection } from '@/stores/products'
+
 import CardProduct from '@/components/CardProduct.vue'
 const UnavailableSection = defineAsyncComponent(() => import('@/components/UnavailableSection.vue'))
 const ProductSectionSkeleton = defineAsyncComponent(
   () => import('@/components/ProductSectionSkeleton.vue')
 )
 
-import { ref, computed, defineAsyncComponent } from 'vue'
+import { defineAsyncComponent } from 'vue'
 
-const dataProduct = ref({
-  title: '',
-  category: '',
-  rating: '',
-  image: '',
-  description: '',
-  price: 0
-})
+const products = new useProductSection()
 
-const currentIdProduct = ref(20)
-const isLoading = ref(false)
-let className = ref({})
-
-const fetchData = async () => {
-  try {
-    const url = `https://fakestoreapi.com/products/${currentIdProduct.value}`
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error('There is no Data:', error)
-    return null
-  }
-}
-
-const fetchDataAndUpdate = async () => {
-  const data = await fetchData()
-  if (data) {
-    dataProduct.value = {
-      title: data.title,
-      category: data.category,
-      rating: data.rating.rate,
-      image: data.image,
-      description: data.description,
-      price: data.price
-    }
-  }
-}
-
-await fetchDataAndUpdate((resolve) => {
+products.currentIdProduct
+products.fetchData()
+products.fetchDataAndUpdate()
+products.showNextSection
+await products.fetchDataAndUpdate((resolve) => {
   setTimeout(() => {
     resolve()
   }, 3000)
 })
-
-const checkCurrentSection = () => {
-  const category = dataProduct.value.category
-  switch (category) {
-    case `men's clothing`:
-      return (className.value = {
-        section: 'men-section',
-        title: 'men-clothes-title',
-        rating: 'circle-men',
-        price: 'men-clothes-price',
-        buttonBuy: 'men-button-buy',
-        buttonNext: 'men-button-next'
-      })
-    case `women's clothing`:
-      return (className.value = {
-        section: 'women-section',
-        title: 'women-clothes-title',
-        rating: 'circle-women',
-        price: 'women-clothes-price',
-        buttonBuy: 'women-button-buy',
-        buttonNext: 'women-button-next'
-      })
-    default:
-      return (className.value = {
-        section: 'unavailable-section',
-        title: 'unavailable-primary-text',
-        rating: 'circle-unavailable',
-        price: 'unvailable-clothes-price',
-        buttonBuy: 'unavailable-button-buy',
-        buttonNext: 'unavailable-button-next'
-      })
-  }
-}
-
-const showNextSection = async () => {
-  isLoading.value = true
-  if (currentIdProduct.value >= 20) {
-    currentIdProduct.value = 0
-  } else {
-    currentIdProduct.value += 1
-  }
-
-  await fetchDataAndUpdate((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 3000)
-  })
-  isLoading.value = false
-}
-
-const circleClasses = computed(() => {
-  const classes = []
-  const rating = Math.round(dataProduct.value.rating)
-  for (let i = 0; i < 5; i++) {
-    classes.push({ circle: true, active: i < rating })
-  }
-  return classes
-})
-
-const formattedRating = computed(() => {
-  return `${dataProduct.value.rating}/5`
-})
 </script>
 
 <template>
-  <UnavailableSection v-if="currentIdProduct === 0" :showNextSection="showNextSection" />
-  <section v-else :class="checkCurrentSection().section" id="background-section">
+  <UnavailableSection
+    v-if="products.currentIdProduct === 0"
+    :showNextSection="products.showNextSection"
+  />
+  <section v-else :class="products.checkCurrentSection().section" id="background-section">
     <CardProduct class="card-section">
-      <img :src="dataProduct.image" id="clothesImage" />
+      <img :src="products.dataProduct.image" id="clothesImage" />
       <article class="clothes-description">
-        <h1 :class="checkCurrentSection().title" id="clothes-title">
-          {{ dataProduct.title }}
+        <h1 :class="products.checkCurrentSection().title" id="clothes-title">
+          {{ products.dataProduct.title }}
         </h1>
         <section class="clothes-categories">
-          <p class="clothes-category">{{ dataProduct.category }}</p>
+          <p class="clothes-category">{{ products.dataProduct.category }}</p>
           <section class="clothes-rating">
-            <p>{{ formattedRating }}</p>
+            <p>{{ products.formattedRating }}</p>
             <div class="rating-indicator">
               <div
-                v-for="(circle, index) in circleClasses"
+                v-for="(circle, index) in products.circleClasses"
                 :key="index"
-                :class="[circle, checkCurrentSection().rating]"
+                :class="[circle, products.checkCurrentSection().rating]"
               ></div>
             </div>
           </section>
         </section>
         <div class="border-bottom"></div>
         <p class="clothes-description">
-          {{ dataProduct.description }}
+          {{ products.dataProduct.description }}
         </p>
         <section class="clothes-footer">
           <div class="border-bottom"></div>
-          <h1 :class="checkCurrentSection().price" id="clothes-price">${{ dataProduct.price }}</h1>
+          <h1 :class="products.checkCurrentSection().price" id="clothes-price">
+            ${{ products.dataProduct.price }}
+          </h1>
           <div class="button-group">
-            <button :class="checkCurrentSection().buttonBuy">Buy now</button>
-            <button @click="showNextSection()" :class="checkCurrentSection().buttonNext">
+            <button :class="products.checkCurrentSection().buttonBuy">Buy now</button>
+            <button
+              @click="products.showNextSection"
+              :class="products.checkCurrentSection().buttonNext"
+            >
               Next Product
             </button>
           </div>
@@ -154,5 +69,5 @@ const formattedRating = computed(() => {
       </article>
     </CardProduct>
   </section>
-  <ProductSectionSkeleton v-if="isLoading" />
+  <ProductSectionSkeleton v-if="products.isLoading" />
 </template>
--- src/components/UnavailableSection.vue
+++ src/components/UnavailableSection.vue
@@ -2,15 +2,14 @@
   <section class="unavailable-section" id="background-section">
     <CardProduct class="unavailable-card-section" style="height: 580px">
       <p class="unavailable-primary-text">This product is unavailable to show</p>
-      <button @click="showNextSection" class="unavailable-button">Next product</button>
+      <button @click="products.showNextSection" class="unavailable-button">Next product</button>
     </CardProduct>
   </section>
 </template>
 
 <script setup>
 import CardProduct from '@/components/CardProduct.vue'
+import { useProductSection } from '@/stores/products'
 
-defineProps({
-  showNextSection: Function
-})
+const products = new useProductSection()
 </script>
